073962d9542f200cc38f8e42ec5a708e86c33f48
Daniil <99849658+RudenkoDaniil@users.noreply.github.com>
2023-03-01 17:40:18 +0100

    Add files via upload

---
+++ Juicy Sweet StoryUITests/Juicy_Sweet_StoryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Juicy_Sweet_StoryUITestsLaunchTests.swift
+//  Juicy Sweet StoryUITests
+//
+//  Created by Даниил on 24.02.2023.
+//
+
+import XCTest
+
+final class Juicy_Sweet_StoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
